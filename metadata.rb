f9d6d1b19efbbe59fe9291b336ccecc14d77784b
David Feeney <dfeeney@gmail.com>
2013-03-11 20:50:21 -0300

    update recipe version number to 0.0.3

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "systems@zepheira.com"
 license          "All rights reserved"
 description      "Installs/Configures viewshare"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 name			 "viewshare"
 
 depends 'application'
